ade96dcf88058f8c2ee449413fc0f25d1a071ac8
Juan David Duran Rangel <duranrangeljuandavid9@gmail.com>
2025-06-15 16:25:04 -0500

    Mejoras visuales en hero section, partículas animadas, ajustes de fuente y estilos

---
+++ js/particles-hero.js
@@ -0,0 +1,60 @@
+ particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 65,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#00f2fe"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.3,
+        "random": true
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 120,
+        "color": "#00ff9d",
+        "opacity": 0.2,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.6,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out"
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false
+        },
+        "resize": true
+      },
+      "modes": {
+        "repulse": {
+          "distance": 100,
+          "duration": 0.4
+        }
+      }
+    },
+    "retina_detect": true
+  });
